4d9a6951585a6ae777b5da0c57398319c888c3f0
Ion Ceban <ceban.ion1992@gmail.com>
2021-04-16 10:01:21 -0500

    Initial Commit

---
+++ BondVillains/VillainCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  VillainCollectionViewCell.swift
+//  BondVillains
+//
+//  Created by Ion Ceban on 4/15/21.
+//  Copyright © 2021 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class VillainCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var villainImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+}
